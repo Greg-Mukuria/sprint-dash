40ac04f6aa35d1fa10d6f8670611cf9d029eb797
Greg <103807051+Greg-Mukuria@users.noreply.github.com>
2023-11-06 06:17:10 -0800

    Update user-settings.tsx

--- components/navigation/user-settings.tsx
+++ components/navigation/user-settings.tsx
@@ -4,10 +4,34 @@ import { Popover, Link } from '@geist-ui/react';
 const UserSettings: React.FC = () => (
   <>
     <Popover.Item title>
-      <span>User Settings</span>
+      <span>Client Settings</span>
     </Popover.Item>
     <Popover.Item>
-      <Link href="#">Teams</Link>
+      <Link href="#">Your Teams</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/adapters">Your Adapters</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/activity">Your Activity</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/security">Security</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/api">API</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/blog">Blog</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/status">System Status</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/contact">Contact Us</Link>
+    </Popover.Item>
+    <Popover.Item>
+      <Link href="/billing">Billing & Services</Link>
     </Popover.Item>
     <Popover.Item>
       <Link href="https://github.com/ofekashery/react-dashboard-design">GitHub</Link>
