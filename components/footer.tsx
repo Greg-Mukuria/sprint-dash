5df52daad0df08c9e051594845a73bb90207ac46
Greg <103807051+Greg-Mukuria@users.noreply.github.com>
2023-11-05 21:38:14 -0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,14 +8,44 @@ const Footer: React.FC = () => {
     <>
       <footer>
         <Text>
-          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="https://github.com/geist-org" target="_blank" rel="noopener" underline>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/privacy" target="_blank" rel="noopener" underline>
             Privacy
           </Link>
-          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="https://github.com/geist-org" target="_blank" rel="noopener" underline>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/activity" target="_blank" rel="noopener" underline>
+            Activity
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/security" target="_blank" rel="noopener" underline>
             Security
           </Link>
-          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="https://github.com/geist-org" target="_blank" rel="noopener" underline>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/terms" target="_blank" rel="noopener" underline>
             Terms
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/status" target="_blank" rel="noopener" underline>
+            Status
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/docs" target="_blank" rel="noopener" underline>
+            Docs
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/contact" target="_blank" rel="noopener" underline>
+            Contact
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/pricing" target="_blank" rel="noopener" underline>
+            Pricing
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/api" target="_blank" rel="noopener" underline>
+            API
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/traders-trust-policy" target="_blank" rel="noopener" underline>
+            Traders Trust Policy
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/blog" target="_blank" rel="noopener" underline>
+            Blog
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/about" target="_blank" rel="noopener" underline>
+            About
+          </Link>
+          <Link style={{paddingLeft: "6px", paddingRight: "6px"}} href="/referrals" target="_blank" rel="noopener" underline>
+            Referrals
           </Link>
         </Text>
       </footer>
