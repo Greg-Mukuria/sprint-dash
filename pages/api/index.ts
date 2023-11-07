ff86725dfb923d1bd250c86d7b364997c141ac81
Greg <103807051+Greg-Mukuria@users.noreply.github.com>
2023-11-07 18:35:12 +0300

    Update index.ts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,4 +1,20 @@
-import React from 'react';
-export default function api() {
-  return(<div>API runnning</div>)
-}
+// Import required modules
+const express = require('express');
+
+// Create an instance of the Express application
+const app = express();
+const port = 3000;
+
+// Define a simple API endpoint
+app.get('/api/message', (req, res) => {
+  const message = {
+    message: 'Hello, this is a simple API endpoint in JavaScript!',
+  };
+
+  res.json(message);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
