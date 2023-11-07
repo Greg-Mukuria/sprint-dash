ddc49618b4651ff46b897d510797d6381c78ddb0
Greg <103807051+Greg-Mukuria@users.noreply.github.com>
2023-11-07 19:07:01 +0300

    Update index.ts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,18 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// Import required modules
-import express from 'express';
-
-// Create an instance of the Express application
-const app = express();
-
-// Define a simple API endpoint
-app.get('/api/message', (req: NextApiRequest, res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   const message = {
     message: 'Hello, this is a simple API endpoint in JavaScript!',
   };
 
   res.json(message);
-});
+};
 
-export default app;
